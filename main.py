c0f4f643325fb3460f409b2dd74a369a296baec2
Lucas Coutinho <l7coutinho.dev@gmail.com>
2024-11-07 12:20:12 -0300

    feat: make some tests

--- main.py
+++ main.py
@@ -1,81 +1,76 @@
 from crewai import Agent, Task, Crew
 from dotenv import load_dotenv
 from langchain_openai import ChatOpenAI
+from crewai_tools import (
+    SerperDevTool,
+    WebsiteSearchTool
+)
 
 # Load environment variables
 load_dotenv()
 
 # LLM
-llm = ChatOpenAI(model="gpt-4o-mini", temperature=0)
+llm = ChatOpenAI(model="gpt-4o-mini", temperature=0, max_tokens=4000)
+
+# Tools
+search_tool = SerperDevTool()
+web_rag_tool = WebsiteSearchTool()
 
 # Agent
 agent_event_locator = Agent(
-    role="Pesquisador de eventos esportivos",
-    goal="Encontrar eventos esportivos de uma determinada categoria com base nos interesses do usuário.",
+    role="Pesquisador de eventos {goal}.",
+    goal="Encontrar eventos musicais em todo Brasil.",
     backstory="""
-        Você sera um pesquisador de eventos esportivos, com amplo conhecimento em eventos esportivos.
-        Sua missão é encontrar eventos esportivos de uma determinada categoria com base nos interesses do usuário
+        Você sera um pesquisador de eventos, com amplo conhecimento em eventos {goal} de diversos generos.
+        Sua missão é encontrar eventos {goal} em todo o Brasil.
     """,
-    llm=llm
+    tools=[search_tool, web_rag_tool],
+    llm=llm,
+    memory=False
 )
 
-planning_agent = Agent(
-    role="Planejador de itinerário",
-    goal="""
-        Criar um itinerário otimizado que maximize a eficiência de deslocamento e minimize o
-        tempo de viagem, considerando preferências do usuário, horários de funcionamento dos
-        locais e opções de transporte disponíveis.
-    """,
-    backstory="""
-        Com sua habilidade em planejar eventos esportivos,
-        você será capaz de criar um itinerário otimizado para encontrar eventos
-        esportivos de uma determinada categoria considerando localização, datas e preferências do usuário.
-    """,
-    llm=llm
-)
+# planning_agent = Agent(
+#     role="Planejador de itinerário",
+#     goal="""
+#         Criar um itinerário otimizado que maximize a eficiência de deslocamento e minimize o
+#         tempo de viagem do usuário.
+#     """,
+#     backstory="""
+#         Com sua habilidade em planejar eventos,
+#         você será capaz de criar um itinerário otimizado que maximize a eficiência de deslocamento e
+#         minimize o tempo de viagem do usuário.
+#     """,
+#     llm=llm,
+#     memory=False
+# )
 
 # Tasks
 task_event_locator = Task(
     description="""
-        Encontrar eventos esportivos de uma determinada categoria com base nos interesses do usuário delimitado entre as tags.
-        Sua resposta final deve ser uma lista de eventos esportivos com informações relevantes, como nome, local, data e hora e uma breve descricão.
-
-        <evento>
-        - Disponibilidade: 20/10/2024 a 24/10/2024
-        - País: Brasil
-        - Evento de interesse: Futebol
-        </evento>
+        Encontrar eventos sobre o tema {goal} em todo brasil no mês de novembro de 2024.
+        Sua resposta final deve ser uma lista de eventos levando em consideração apenas as informações contidas no site,
+		contendo informações relevantes, como nome, data e local.
     """,
-    expected_output="""Lista de eventos esportivos contendo:
-        - Nome do evento
-        - Local
-        - Data e hora
-        - Breve descrição
-    """,
+    expected_output="Lista de eventos contendo: Nome do evento, data do evento e Local do evento",
     agent=agent_event_locator
 )
 
-task_planning_agent = Task(
-    description="""
-        Criar um itinerário otimizado que maximize a eficiência de deslocamento e minimize o tempo de viagem do usuário.
-        Inclua recomendações de transportees e horários de funcionamento.
-        A resposta final deve ser um plano de viagem completo, com um cronograma diário e um itinerário otimizado.
-    """,
-    expected_output="""Plano de viagem detalhado contendo:
-        - Cronograma diário
-        - Itinerário otimizado
-        - Recomendações de transporte
-        - Horários de funcionamento
-    """,
-    agent=planning_agent
-)
+# task_planning_agent = Task(
+#     description="""
+#         Criar um itinerário otimizado que maximize a eficiência de deslocamento e minimize o tempo de viagem do usuário.
+#         Inclua recomendações de transportes e horários de funcionamento.
+#     """,
+#     expected_output="Itinerário contendo sugestãos de transporte e horários de funcionamento.",
+#     agent=planning_agent
+# )
 
 # Crew
 crew = Crew(
-    agents=[agent_event_locator, planning_agent],
-    tasks=[task_event_locator, task_planning_agent],
+    agents=[agent_event_locator],
+    tasks=[task_event_locator],
     verbose=True
 )
+
 
 result_output = crew.kickoff()
  
